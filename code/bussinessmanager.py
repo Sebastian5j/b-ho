2859e3d4dee4be02a545e02e80939ac26c4237cf
sebas <sebastianqjuarez@gmail.com>
2023-11-28 19:02:38 -0600

    ahora paso las cosas para el buho legal en una API aparte

--- code/bussinessmanager.py
+++ code/bussinessmanager.py
@@ -1,88 +1,38 @@
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
+MY_ENV_VAR = os.getenv('MY_ENV_VAR')
 
-class Subsystem1:
-    """
-    The Subsystem can accept requests either from the facade or client directly.
-    In any case, to the Subsystem, the Facade is yet another client, and it's
-    not a part of the Subsystem.
-    """
+class BuhoLegal:
+    def get_token(self):
+        credenciales = { 'username' : 'sebastianqjuarez@gmail.com', 'password' : 'p4r4BuhoLegal' }
+        respose = requests.post('https://api.buholegal.com/apikey/', data=credenciales) 
+        if respose.status_code == 200:
+            token = respose.json()['token']
+            print(token)
+            self.token = token
+        else:
+            raise Exception("No pude obtener el token necesario")
 
-    def operation1(self) -> str:
-        return "Subsystem1: Ready!"
-
-    # ...
-
-    def operation_n(self) -> str:
-        return "Subsystem1: Go!"
-
-
-class Subsystem2:
-    """
-    Some facades can work with multiple subsystems at the same time.
-    """
-
-    def operation1(self) -> str:
-        return "Subsystem2: Get ready!"
-
-    # ...
-
-    def operation_z(self) -> str:
-        return "Subsystem2: Fire!"
+class Manager:
     
-class Facade:
-    """
-    The Facade class provides a simple interface to the complex logic of one or
-    several subsystems. The Facade delegates the client requests to the
-    appropriate objects within the subsystem. The Facade is also responsible for
-    managing their lifecycle. All of this shields the client from the undesired
-    complexity of the subsystem.
-    """
+    def __init__(self, buho: BuhoLegal) -> None:
+        self._buho = buho or BuhoLegal()
 
-    def __init__(self, subsystem1: Subsystem1, subsystem2: Subsystem2) -> None:
-        """
-        Depending on your application's needs, you can provide the Facade with
-        existing subsystem objects or force the Facade to create them on its
-        own.
-        """
-
-        self._subsystem1 = subsystem1 or Subsystem1()
-        self._subsystem2 = subsystem2 or Subsystem2()
-
-    def operation(self) -> str:
-        """
-        The Facade's methods are convenient shortcuts to the sophisticated
-        functionality of the subsystems. However, clients get only to a fraction
-        of a subsystem's capabilities.
-        """
-
-        results = []
-        results.append("Facade initializes subsystems:")
-        results.append(self._subsystem1.operation1())
-        results.append(self._subsystem2.operation1())
-        results.append("Facade orders subsystems to perform the action:")
-        results.append(self._subsystem1.operation_n())
-        results.append(self._subsystem2.operation_z())
-        return "\n".join(results)
-
-
-
-
-def client_code(facade: Facade) -> None:
-    """
-    The client code works with complex subsystems through a simple interface
-    provided by the Facade. When a facade manages the lifecycle of the
-    subsystem, the client might not even know about the existence of the
-    subsystem. This approach lets you keep the complexity under control.
-    """
-
-    print(facade.operation(), end="")
+    def get_token(self) -> str:
+        token = self._buho.get_token()
+        return token
+    
+def client_code(facade: Manager) -> None:
+    facade.get_token()
 
 
 if __name__ == "__main__":
     # The client code may have some of the subsystem's objects already created.
     # In this case, it might be worthwhile to initialize the Facade with these
     # objects instead of letting the Facade create new instances.
-    subsystem1 = Subsystem1()
-    subsystem2 = Subsystem2()
-    facade = Facade(subsystem1, subsystem2)
-    client_code(facade)
+    buho = BuhoLegal()
+    manager = Manager(buho= buho)
+    client_code(manager)
