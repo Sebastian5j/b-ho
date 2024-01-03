c1f51c918948db246faef26c5277c5354de0fc66
Luis199706 <villanuevaluis20@gmail.com>
2024-01-02 22:43:47 -0600

    Nuevas funciones para los request

--- bussinessmanager.py
+++ bussinessmanager.py
@@ -3,12 +3,19 @@ import os, json
 from dotenv import load_dotenv
 load_dotenv()
 import my_tools
-USER                = os.getenv('USER_BUHO_LEGAL')
-PASSWORD            = os.getenv('PASSWORD')
-API_BASE            = os.getenv('API_BASE')
-BUHO_CREDENTIALS    = os.getenv('BUHO_CREDENTIALS')
-API_CREATE_USER     = os.getenv('API_CREATE_USER')
-API_GET_TOKEN       = os.getenv('API_GET_TOKEN')
+USER                    = os.getenv('USER_BUHO_LEGAL')
+PASSWORD                = os.getenv('PASSWORD')
+API_BASE                = os.getenv('API_BASE')
+BUHO_CREDENTIALS        = os.getenv('BUHO_CREDENTIALS')
+API_CREATE_USER         = os.getenv('API_CREATE_USER')
+API_GET_TOKEN           = os.getenv('API_GET_TOKEN')
+API_GET_ESTADOS         = os.getenv('API_GET_ESTADOS')
+API_BUSQUEDA_EXPEDIENTE = os.getenv('API_BUSQUEDA_EXPEDIENTE')
+API_HIST_BUSQUEDAS      = os.getenv("API_HIST_BUSQUEDAS")
+API_VALIDAR_CURP        = os.getenv("API_VALIDAR_CURP")
+API_GET_CEDULAS         = os.getenv("API_GET_CEDULAS")
+
+ERROR_MESSAGE = f"Error al enviar request."
 
 logger              = my_tools.get_a_logger()
 
@@ -53,6 +60,102 @@ class BuhoLegal:
         respose = requests.post(API_CREATE_USER, data=data, headers=headers)
         logger.debug(f"response: {respose.json()}")
         return respose.json()
+    
+    
+    def get_estados(self):
+        """
+        El objetivo de esta funcionalidad es permitir a los usuarios conocer 
+        las abreviaturas que se utilizan para referirse a cada estado a la 
+        hora de realizar una búsqueda.
+        """
+        logger.debug(f"Obteniendo estados...")
+        try:
+            response = requests.get(API_GET_ESTADOS, headers = {'Authorization':'Token ' + self.__token})
+            if response.status_code == 200:
+                logger.info(f"Estados recuperados!")
+                return response.json()
+            else:
+                logger.error(f"No se pueden obtener los estados.")
+                raise Exception(f"No se pueden obtener los estados.")
+        except:
+            logger.error(ERROR_MESSAGE)
+            raise Exception(ERROR_MESSAGE)
+        
+        
+    def busqueda(self):
+        logger.debug(f"Realizando búsqueda de expedientes...")
+        PARAMS = {}
+        PARAMS['nombre'] = 'juan'
+        PARAMS['paterno'] = 'mengano'
+        PARAMS['materno'] = 'sultano'
+        PARAMS['persona'] = 'fisica'
+        PARAMS['estado'] = 'AS' # AS se refiere a Aguascalientes. PARAMS['detalle'] = True
+        PARAMS['detalle'] = True
+        PARAMS['fecha_inicio'] = '01-01-2019' 
+        PARAMS['fecha_fin'] = '01-06-2020' 
+        PARAMS['curp'] = 'ASDF801332GYBHQL00'
+        try:
+            response = requests.get(API_BUSQUEDA_EXPEDIENTE, headers = {'Authorization':'Token ' + self.__token}, params=PARAMS)
+            if response.status_code == 200:
+                logger.info("Obteniendo expediente!")
+                return response.json()
+            else:
+                logger.error(f"No se puede obtener el expediente.")
+                raise Exception(f"No se puede obtener el expediente.")
+        except:
+            logger.error(ERROR_MESSAGE)
+            raise Exception(ERROR_MESSAGE)
+            
+    def get_busquedas(self):
+        logger.debug(f"Consultando el historial de busquedas...")
+        PARAMS = {}
+        PARAMS['fecha_inicio'] = '01-01-2019' 
+        PARAMS['fecha_fin'] = '01-06-2020'
+        try:
+            response = requests.get(API_HIST_BUSQUEDAS, headers = {'Authorization':'Token ' + self.__token}, params=PARAMS)
+            if response.status_code == 200:
+                logger.info(f"Obteniendo historial de busquedas!")
+                return response.json()
+            else:
+                logger.error(f"No se puede obtener el historial de busquedas.")
+                raise Exception(f"No se puede obtener el historial de busquedas.")
+        except:
+            logger.error(ERROR_MESSAGE)
+            raise Exception(ERROR_MESSAGE)
+        
+    def validar_curp(self):
+        logger.debug(f"Validando CURP..")
+        try:
+            response = requests.get(API_VALIDAR_CURP, headers = {'Authorization':'Token ' + self.__token}, params={'curp':'VIAL885764656465'})
+            if response.status_code == 200:
+                logger.info(f"CURP validada!")
+                return response.json()
+            else:
+                logger.error(f"No se puede validar la CURP.")
+                raise Exception(f"No se puede validar la CURP.")
+        except:
+            logger.error(ERROR_MESSAGE)
+            raise Exception(ERROR_MESSAGE)
+        
+    def get_cedulas(self):
+        logger.debug(f"Obteniendo cedulas profesionales..")
+        PARAMS = {'nombre' : 'Juan', 'paterno' : 'Mengano', 'materno' : 'Sultano'}
+        try:
+            response = requests.get(API_GET_CEDULAS, headers = {'Authorization':'Token ' + self.__token}, params=PARAMS)
+            if response.status_code == 200:
+                logger.info(f"Cedulas obtenidas!")
+                return response.json()
+            else:
+                logger.error(f"No se pueden obtener las cedulas.")
+                raise Exception(f"No se pueden obtener las cedulas.")
+        except:
+            logger.error(ERROR_MESSAGE)
+            raise Exception(ERROR_MESSAGE)
+                
+                
+            
+        
+        
     
 class Manager:
     
